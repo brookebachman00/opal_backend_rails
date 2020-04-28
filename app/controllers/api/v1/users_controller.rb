53676290ccaaacfe92cdadc09045d508fecc395e
brookebachman00 <yourpetlover4411@yahoo.com>
2020-04-27 18:30:10 -0700

    fixed user and auth controller.rb

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,41 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_action :authorized, only: [:create]
   
-    def create
-    
-    
-        @user = User.create(user_params)
-        if @user.valid?
-          
-          render json: { user: UserSerializer.new(@user) }, status: :created
-        else
-          render json: { error: 'failed to create user' }, status: :not_acceptable
-        end
-      end
+  def index
+    @users = User.all
+    render json: @users
+  end
+  # GET /users/1
+  def show
+    render json: @user
+  end
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      @token = encode_token({ user_id: @user.id })
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+  end
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+  end
+  
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
      
       private
       def user_params
